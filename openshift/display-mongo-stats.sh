658fffa66f6361b307746a3dc9404200a983f2b5
Lokesh Rangineni <lokeshforjava@gmail.com>
2022-02-05 00:37:09 -0500

    checking script to display mongo number of records and some openshift yaml file to create mongo replicaset and sharded cluster.

--- openshift/display-mongo-stats.sh
+++ openshift/display-mongo-stats.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+## Usage -  ./display-mongo-stats.sh mongodb-benchmark-replica-set-0
+
 MONGO_POD_NAME=$1   # Mongo Pod Name from Open Shift Cluster
 
 oc cp display-mongo-stats.js $MONGO_POD_NAME:/tmp
-oc exec $MONGO_POD_NAME -- bash -c "/var/lib/mongodb-mms-automation/mongodb-linux-x86_64-4.4.0-ent/bin/mongo < /tmp/delete-existing-collection.js"
+oc exec $MONGO_POD_NAME -- bash -c "/var/lib/mongodb-mms-automation/mongodb-linux-x86_64-4.4.0-ent/bin/mongo < /tmp/display-mongo-stats.js"
